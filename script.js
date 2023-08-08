bbdf1c6e4e72a107e5aaa15c063f1b3ef6f981f4
Teja7789 <89193972+Teja7789@users.noreply.github.com>
2023-08-08 10:25:18 +0530

    index.html file is added

---
+++ script.js
@@ -0,0 +1,59 @@
+function showHeadingAndCheck(headId,optioId) {
+   
+    const head = document.getElementById('head1');
+    head.style.display='block';
+   
+    const radioOption = document.getElementById('option1');
+  
+    radioOption.checked = true;
+  }
+
+  function hideHeadingAndUncheck(headId,optioId) {
+
+    const head = document.getElementById('head1');
+    head.style.display='none';
+    const radioOption = document.getElementById('option1');
+
+    radioOption.checked = false;
+  }
+
+  function showHeadingAndCheckTwo(headId,optioId) {
+   
+    const head2 = document.getElementById('head2');
+    head2.style.display='block';
+       const radioOption2 = document.getElementById('option2');
+   
+    radioOption2.checked = true;
+  }
+
+  function hideHeadingAndUncheckTwo(headId,optioId) {
+   
+    const head2 = document.getElementById('head2');
+    head2.style.display='none';
+    
+    const radioOption2 = document.getElementById('option2');
+    
+    radioOption2.checked = false;
+  }
+  
+  function showHeadingAndCheckThree(headId,optioId) {
+    
+    const head3 = document.getElementById('head3');
+    head3.style.display='block';
+    
+    const radioOption3 = document.getElementById('option3');
+
+    radioOption3.checked = true;
+  }
+
+  function hideHeadingAndUncheckThree(headId,optioId) {
+   
+    const head3 = document.getElementById('head3');
+    head3.style.display='none';
+   
+    const radioOption3 = document.getElementById('option3');
+  
+    radioOption3.checked = false;
+  }
+
+
